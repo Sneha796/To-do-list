ff8fc6eb54686b45e88da00d70c74c946a08dfa7
Sneha Nikam <snehanikam796@gmail.com>
2026-08-02 00:45:16 +0530

    Refactor addValue function to create list items

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function addValue() {
   let add = 
   document.getElementById("screen").value;
-  document.getElementById("taskScreen").innerHTML += add ;
-}
-
-function removeValue() {
-  let taskScreen = 
-  document.getElementById("taskScreen").value =" ";
+  let task = document.createElement("li");
+  task.textContent = add;
+  task.onclick = function() {
+    task.remove();
+  };
+  document.getElementById("taskScreen").appendChild(task);
+  document.getElementById("screen").value = "";
 }
